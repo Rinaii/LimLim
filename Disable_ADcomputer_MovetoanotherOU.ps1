18a483295bec0588432872e0da06c19de245fdff
Rinaii <120522425+Rinaii@users.noreply.github.com>
2025-01-13 14:58:18 -0500

    Add files via upload
    This script allows you to disable any computer from a text file and move it to a disabled OU.

---
+++ Disable_ADcomputer_MovetoanotherOU.ps1
@@ -0,0 +1,7 @@
+﻿$pclist = Get-Content C:\Temp\DisableUsers.txt
+$targetOU = "OU=Computers,OU=Disabled,DC=Contoso,DC=com"  
+foreach($pc in $pclist){  
+    if ($pc.Trim()) {  
+        Get-ADComputer $pc.Trim() | Disable-ADAccount -PassThru | Move-ADObject -TargetPath $targetOU  
+    }  
+}
