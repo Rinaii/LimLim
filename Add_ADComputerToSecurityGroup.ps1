d79da15c9286e54e8575ade18306b059e8204fd8
Rinaii <120522425+Rinaii@users.noreply.github.com>
2025-01-13 14:29:17 -0500

    Add files via upload
    This script will allow you to add ad computers to any security group through get-content.

---
+++ Add_ADComputerToSecurityGroup.ps1
@@ -0,0 +1,7 @@
+﻿#Add-AdGroupmember -ID $O365_Intune_Enrollment -Members {Get-ADComputer (get-content C:\temp\remoteacess.txt)}
+$Computers = Get-Content -Path ‘C:\temp\Remoteaccess.txt’
+foreach ($computer in $computers)
+{$dns=get-adcomputer $computer
+$b=$dns.distinguishedname
+Add-ADPrincipalGroupMembership $b ‘O365_Intune_Enrollment’
+}
