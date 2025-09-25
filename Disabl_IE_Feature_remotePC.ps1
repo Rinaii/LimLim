77b7312fb1cfb55f305a0d8a43a443e93e69849f
Rinaii <120522425+Rinaii@users.noreply.github.com>
2025-09-25 11:21:11 -0400

    Add files via upload
    This script will disable internet explorer on nay remoted computer on prem.

---
+++ Disabl_IE_Feature_remotePC.ps1
@@ -0,0 +1,12 @@
+﻿#$PC = get-content C:\Kode\Explorer\PCNames.txt #Get a list of computer from C:\Kode\explorer
+#Foreach ($pc in $computers){
+try {
+    Invoke-Command -ComputerName 500-PC -ScriptBlock {
+    Disable-windowsoptionalfeature -online -res -featureName Internet-Explorer-Optional-amd64
+    #dism /online /Disable-Feature /FeatureName:Internet-Explorer-Optional-amd64 -force
+    Restart-Computer $PC -force
+        } #-erroraction Stop | out-file C:\Kode\Explorer\failed.txt
+}
+catch {
+    $_.Exception.Message | Out-File C:\Kode\Explorer\failed.txt
+}
