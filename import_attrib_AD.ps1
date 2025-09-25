2c0db0917f2602d8689aaeadc89ce42d230aa75b
Rinaii <120522425+Rinaii@users.noreply.github.com>
2025-09-25 12:00:15 -0400

    Add files via upload
    This script will pull users on prem attributes based on specify OU.

---
+++ import_attrib_AD.ps1
@@ -0,0 +1,4 @@
+﻿Import-Module activedirectory
+Get-ADUser -searchbase 'OU=Users,OU=IT,DC=Kode,DC=com' -Filter * –Properties UserPrincipalName, givenname, surname, department, title| Export-Csv -Path C:\Kode\UserAttributes.csv 
+#company, department, title, telephoneNumber, PwdLastSet | Select-Object SamAccountName, Name, company, department, title, telephoneNumber
+#Get-ADUser -SearchBase "OU=HR,DC=SHELLPRO,DC=LOCAL" -Filter * -Properties Name
