1737ad9c462701743f35516231c12c0bff8c76c9
Rinaii <120522425+Rinaii@users.noreply.github.com>
2025-09-25 11:32:27 -0400

    Add files via upload
    This script will disable users and move its to a disabled OU.

---
+++ Disable_ADUser_Move2_OU.ps1
@@ -0,0 +1,7 @@
+﻿$pclist = Get-Content C:\Kode\DisableUsers.txt
+$targetOU = "OU=Users,OU=Disabled,DC=kode,DC=com"  
+foreach($pc in $pclist){  
+    if ($pc.Trim()) {  
+        Get-ADUser $pc.Trim() | Disable-ADAccount -PassThru | Move-ADObject -TargetPath $targetOU  
+    }  
+}
