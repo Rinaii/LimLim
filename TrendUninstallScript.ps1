f3a6ebc205e005bcbb844eba1b181c28a6cf5910
Rinaii <120522425+Rinaii@users.noreply.github.com>
2025-09-25 14:39:39 -0400

    Add files via upload
    This script will uninstall Trend Micro Apex One agent using Foreach condition, try & catch (psexec).
    You will need a specific SCUT.exe file that is provided by Trend Support.

---
+++ TrendUninstallScript.ps1
@@ -0,0 +1,16 @@
+﻿$PSExecPath = "C:\Kode\psexec.exe"
+Set-Alias -Name psexec -Value $PSExecPath
+$source = "\\Kode\main\Installs\Trend_Uninstall"
+$destination = "\\$computer\c$\Kode"
+$computers = Get-Content -path "\\Kode\main\Installs\Trend_uninstall\RehabPCTrend.txt"
+write-host "Start copying the files from '\Kode\main\Installs\Trend_uninstall\*' to all computers" -ForegroundColor Green
+foreach ($computer in $computers){
+copy-item -path "\\Kode\main\Installs\Trend_uninstall\*" -destination "\\$computer\c$\Kode" -verbose
+write-host $computer "Running the Scut.exe file to uninstall Trend Micro Apex one Security Agent" -foregroundcolor Yellow
+try{ 
+psexec -s \\$computer cmd.exe /c "C:\Kode\SCUT.exe -noinstall -dbg"
+write-host "$computer was successfully uninstalled Trend Micro."
+Remove-Item -Path \\$computer\c$\Kode\SCUT.exe, \\$computer\c$\Kode\RehabPCTrend.txt}
+catch {
+write-host "$computer Failled to uninstall Trend." -ForegroundColor DarkRed | Export-csv -Path "C:\Kode\FailedRapid7PC.csv" -Append -NoTypeInformation }
+}
