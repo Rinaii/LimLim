dee000d0be8bd7f1d9623c343a1cada85b0164c3
Rinaii <120522425+Rinaii@users.noreply.github.com>
2025-09-25 12:10:47 -0400

    Add files via upload
    This script will pull any "Enable" windows 7 computer from a specify OU based on last active day (Day that you set).

---
+++ InactiveADcomputer.ps1
@@ -0,0 +1,5 @@
+﻿$domain = "kode.com"  
+$DaysInactive = 15
+$time = (Get-Date).Adddays(-($DaysInactive))
+$oldPC = Get-ADcomputer -Filter {(whenchanged -lt $time) -and (OperatingSystem -Like "Windows 7*") -and (Enabled -eq $true) -and (Name -notlike "*survey*")} -SearchBase "OU=Intune Testing,OU=Computers,OU=IT,DC=Kode,DC=com" -Properties OperatingSystem, LastLogonDate, modified, description, whenchanged | Export-Csv -Path C:\Kode\ITOU.csv -Force -NoTypeInformation
+#$oldUsers | select Userprincipalname, modified, whenchanged, DistinguishedName, description | sort modified | Export-Csv -Path C:\Kode\Disable_User_60\6monthsPC.csv -Force -NoTypeInformation
