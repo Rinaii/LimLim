0412dff920bf8afdd8ee0bf2a9d468809c99a63a
Rinaii <120522425+Rinaii@users.noreply.github.com>
2025-09-25 14:12:32 -0400

    Add files via upload
    This is a PowerShell script written in purposed of uninstall Adobe Flash Player from Windows computer.

---
+++ Uninstall-Adobe.ps1
@@ -0,0 +1,19 @@
+﻿#This is a powershell script written in purposed of uninstall Adobe Flash Player from Window computer
+#Remediation Project (InsightVM)
+#(\\Kode\main\Installs\Adobe Uninstall\*) Consists of 3 files, machines_need_uninstall, Uninstall_flash_player, and uninstallflash.bat.
+
+$PSExecPath = "C:\Kode\psexec.exe"
+$source = "\\Kode\main\Installs\Adobe Uninstall"
+$destination = "\\$computer\c$\Laurel"
+#Insert the file name "PC_Uninstall.txt" instead of test1.txt
+$computers = Get-Content -path "\\Kode\main\Installs\Adobe Uninstall\PC_adobetest.txt"
+write-host "Start copying the files from '\Kode\main\Installs\Adobe Uninstall\*' to all computers" -ForegroundColor Green
+foreach ($computer in $computers){
+copy-item -path "\\Kode\main\Installs\Adobe Uninstall\*" -destination "\\$computer\c$\laurel" -verbose
+write-host $computer "Running the UninstallFlash.bat file to uninstall Adobe Flash Play" -foregroundcolor Yellow
+try { Invoke-command -computername $computer -scriptblock { & C:\Kode\uninstallflash.bat } -ErrorAction stop}
+catch { Set-Alias -Name psexec -Value $PSExecPath
+& psexec -s \\$computername cmd.exe /c "C:\Kode\uninstallflash.bat" }
+Remove-Item -Path C:\Windows\System32\Macromed\Flash* -Verbose
+Remove-Item -Path \\$computer\c$\Kode\uninstall_flash_player.exe, \\$computer\c$\Kode\uninstallflash.bat, \\$computer\c$\Kode\PC_adobetest.txt
+}
