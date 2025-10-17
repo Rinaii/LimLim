4b49bad7683b5df1057d61daea6549271c1bd871
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-17 14:54:37 -0400

    Add files via upload
    This PowerShell script retrieves a list of all computer accounts from a specific Organizational Unit (OU) in Active Directory and displays selected properties for each one

---
+++ Powershell Scripts/Export-ADComputers.ps1
@@ -0,0 +1,4 @@
+﻿﻿Import-Module activedirectory
+Get-ADComputer -SearchBase 'OU=Rehab,OU=Computers,OU=Norworth,OU=Facilities,DC=momotecho,DC=com' -Filter * -Property * |
+Select-Object Name,OperatingSystem,LastLogonDate,DistinguishedName |
+Export-CSV "C:\MoMo\Norworth Migration\ADcomputersNor-Reh.csv" -NoTypeInformation -Encoding UTF8
