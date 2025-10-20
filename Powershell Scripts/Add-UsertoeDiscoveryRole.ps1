e2d8125060fea17072c72c64688aad83e64d59c3
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-20 14:59:22 -0400

    Add files via upload

---
+++ Powershell Scripts/Add-UsertoeDiscoveryRole.ps1
@@ -0,0 +1,9 @@
+﻿Update-Module ExchangeOnlineManagement
+Import-Module ExchangeOnlineManagement
+#UNP An admin account that has sufficient privilege to make this change
+$UPN = 'moedadmin365@nuhh.onmicrosoft.com'
+Connect-ExchangeOnline -UserPrincipalName $UPN
+Connect-IPPSSession -UserPrincipalName  $UPN
+#UNP1 the user that you would like to add to eDiscoveryManager Role.
+$UPN1 = 'jjadmin365@nuhh.onmicrosoft.com'
+Add-RoleGroupMember -identity "eDiscoveryManager" -Member $UPN1
