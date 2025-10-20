d89e0d7176ad896415f0cb67b0ac6a6710a093d3
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-20 14:58:51 -0400

    Add files via upload

---
+++ Powershell Scripts/Add-UserSecurityGroup.ps1
@@ -0,0 +1,8 @@
+﻿#add any users from Get-content to a security membership group using Foreach
+$users=import-csv c:\temp\Remoteaccess.txt
+foreach($user in $users)
+{
+$upn = $user.upn
+get-aduser -filter "userprincipalname -eq '$upn'" |
+% {add-adgroupmember -identity "Intune Prod Users" -members $_} -Verbose
+}
