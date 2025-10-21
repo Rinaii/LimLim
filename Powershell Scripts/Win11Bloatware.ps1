dc2db062bae02b862a00a2e6c80c104b47d9111b
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-20 21:33:18 -0400

    Add files via upload

---
+++ Powershell Scripts/Win11Bloatware.ps1
@@ -0,0 +1,50 @@
+﻿# List of keywords to search for in app names
+$AppsToRemove = @(
+    "Microsoft.Copilot",
+    "Microsoft.BingNews",
+    "Microsoft.GamingApp",
+    "Microsoft.WindowsFeedbackHub",
+    "Microsoft.BingSearch",
+    "Microsoft.BingWeather", 
+    "DolbyLaboratories.DolbyAccess",
+    "Microsoft.PowerAutomateDesktop",
+    "Microsoft.WindowsSoundRecorder",
+    "Microsoft.GetHelp",
+    "Microsoft.ZuneMusic",
+    "Microsoft.XboxSpeechToTextOverlay",
+    "Microsoft.XboxIdentityProvider",
+    "Microsoft.XboxGamingOverlay",
+    "Microsoft.Xbox.TCUI",
+    "Microsoft.Windows.DevHome",
+    "Microsoft.MicrosoftSolitaireCollection",
+    "Microsoft.WindowsAlarms",
+    "Clipchamp.Clipchamp",
+    "Microsoft.XboxGameCallableUI",
+	"Microsoft.YourPhone",                            
+    "MicrosoftCorporationII.QuickAssist"
+)
+
+foreach ($Keyword in $AppsToRemove) {
+    # Remove for current user
+    $UserPackages = Get-AppxPackage | Where-Object { $_.Name -like "*$Keyword*" }
+    foreach ($Package in $UserPackages) {
+        Write-Output "Removing app for current user: $($Package.Name)"
+        Remove-AppxPackage -Package $Package.PackageFullName -ErrorAction SilentlyContinue
+    }
+
+    # Remove provisioned packages (for new users)
+    $ProvisionedPackages = Get-AppxProvisionedPackage -Online | Where-Object { $_.DisplayName -like "*$Keyword*" }
+    foreach ($ProvPackage in $ProvisionedPackages) {
+        Write-Output "Removing provisioned app: $($ProvPackage.DisplayName)"
+        Remove-AppxProvisionedPackage -Online -PackageName $ProvPackage.PackageName -ErrorAction SilentlyContinue
+    }
+
+    # Remove for all users (if already installed for others)
+    $AllUserPackages = Get-AppxPackage -AllUsers | Where-Object { $_.Name -like "*$Keyword*" }
+    foreach ($Package in $AllUserPackages) {
+        Write-Output "Removing app for all users: $($Package.Name)"
+        Remove-AppxPackage -Package $Package.PackageFullName -AllUsers -ErrorAction SilentlyContinue
+    }
+}
+
+Write-Output "Bloatware removal complete for all users and future users."
