b70eadf32d0c74b32dd8faf5aa438e1a84a3fe9e
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-20 21:26:29 -0400

    Add files via upload

---
+++ Powershell Scripts/FindAdppInstallOnPC.ps1
@@ -0,0 +1,16 @@
+﻿$PCs = get-content C:\Temp\SophosPC.txt
+foreach ($PC in $PCs) {
+
+$MyApp = Get-WmiObject -Class Win32_Product | sort-object Name | select Name | where { $_.Name -match "Sophos Agent"}
+
+if ($MyApp -match "Sophos Agent")
+{
+    #Write-Output $MyApp.Name
+    Write-host "Sophos Agent is sitll on $PC." -ForegroundColor Green
+    $fails | export-csv C:\Temp\SophosFailed.txt -NoTypeInformation
+}
+else
+{
+    Write-host "Sophos agent is GONE." -ForegroundColor DarkYellow
+}
+}
