11482c348ecb65c473eb8a58a658e34cd7b1f3d0
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-20 21:47:57 -0400

    Add files via upload

---
+++ Powershell Scripts/Enable-WMI-Remote.ps1
@@ -0,0 +1,13 @@
+﻿# Enable WinRM service and set it to start automatically
+Set-Service WinRM -StartupType Automatic
+Start-Service WinRM
+
+# Configure WinRM for quick configuration
+winrm quickconfig -quiet
+
+# Enable firewall rules for WMI
+netsh advfirewall firewall set rule group="Windows Management Instrumentation (WMI)" new enable=yes
+
+# Optional: Configure TrustedHosts if the remote computer is in a workgroup
+# Set-Item WSMan:\localhost\Client\TrustedHosts -Value "*" -Force 
+# Use with caution as it allows connections from any host
