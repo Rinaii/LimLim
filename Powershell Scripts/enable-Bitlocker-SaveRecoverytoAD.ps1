83f53d563c9c1e66f98c1f6905768c13d8288768
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-20 21:48:29 -0400

    Add files via upload

---
+++ Powershell Scripts/enable-Bitlocker-SaveRecoverytoAD.ps1
@@ -0,0 +1,13 @@
+﻿# Enable BitLocker on the C drive with a recovery password
+Enable-BitLocker -MountPoint "C:" -RecoveryPasswordProtector -SkipHardwareTest
+
+# Get the BitLocker volume information
+$bitlockerVolume = Get-BitLockerVolume -MountPoint "C:"
+
+# Get the recovery key protector
+$recoveryProtector = $bitlockerVolume.KeyProtector | Where-Object {$_.KeyProtectorType -eq 'RecoveryPassword'}
+
+# Backup the recovery key to Active Directory
+Backup-BitLockerKeyProtector -MountPoint "C:" -KeyProtectorId $recoveryProtector.KeyProtectorId
+
+Write-Host "BitLocker has been enabled and the recovery key has been backed up to Active Directory."
