230551ea343103ad5203b89a5934712a1c40dd7a
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-20 21:26:01 -0400

    Add files via upload

---
+++ Powershell Scripts/Disable_ADcomputer_Move2OU.ps1
@@ -0,0 +1,7 @@
+﻿$pclist = Get-Content C:\Kode\DisableUsers.txt
+$targetOU = "OU=Computers,OU=Disabled,DC=Kode,DC=com"  
+foreach($pc in $pclist){  
+    if ($pc.Trim()) {  
+        Get-ADComputer $pc.Trim() | Disable-ADAccount -PassThru | Move-ADObject -TargetPath $targetOU  
+    }  
+}
