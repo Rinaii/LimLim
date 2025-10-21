eb7d4fc11bf524f7dc935b6f64f8c2128d0fc580
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-20 21:26:52 -0400

    Add files via upload

---
+++ Powershell Scripts/Get-FileHash-fromComputer.ps1
@@ -0,0 +1,13 @@
+﻿#Invoke-SSH -ComputerName 00-kode12 -Command "Get-FileHash -Path "c:\Users\37-hub100343\AppData\Local\OneStart.ai\OneStart\Application" -Algorithm SHA256"
+$computerName = "00-kode12"
+$filePath = "C:\Users\02-MAS100182\AppData\Local\OneStart.ai\OneStart\Application\132.0.6834.161\Installer\setup.exe" #File Path
+$hashAlgorithm = "SHA256"
+
+Invoke-Command -ComputerName $computerName -ScriptBlock {
+    param($filePath, $hashAlgorithm)
+    if (Test-Path -Path $filePath) {
+        Get-FileHash -Path $filePath -Algorithm $hashAlgorithm | Format-List
+    } else {
+        Write-Warning "File not found: $filePath"
+    }
+} -ArgumentList $filePath, $hashAlgorithm
