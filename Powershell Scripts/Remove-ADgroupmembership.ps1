aebf68d6aad41d11e5b42fcfc45f785b3d9732e9
Lim/Rini <120522425+Rinaii@users.noreply.github.com>
2025-10-20 21:28:58 -0400

    Add files via upload

---
+++ Powershell Scripts/Remove-ADgroupmembership.ps1
@@ -0,0 +1,6 @@
+﻿$users= get-content c:\laurel\remoteaccess.txt
+foreach($user in $users){
+
+Remove-ADGroupMember -Identity "EOP1 Users" -Members $user
+write-host "$user has been removed to Intube Prod User" -ForegroundColor Yellow
+}
